fcc5aabd210d95679fbd6cb1d251f804afd41c66
Amaury <amaastier@gmail.com>
2017-02-23 19:46:24 +0100

    Start parsing

--- finals/project_cpp/src/hashcode2017.cpp
+++ finals/project_cpp/src/hashcode2017.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <cstdlib>
 #include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+
+//Struct
+typedef struct latency_cache{
+	unsigned int id_cache;
+	unsigned int latency_ms;
+}latency_cache;
+
+typedef struct request{
+	unsigned int id_video;
+	unsigned int nb_ask;
+}latency_cache;
 
 enum node_type {ENDPOINT, CACHE, DATA_CENTER};
 
@@ -22,8 +38,20 @@ typedef struct cache{
 
 
 
-
-int main() {
-
-	return 0;
+int main(int argc, char** argv)
+{
+	if(argc != 3)
+	{
+		std::cout<<"Pass more arguments: <File_input> <File_output>"<<std::endl;
+		exit(1);
+	}
+	//Open File
+	std::ifstream in_f;
+	std::string in_f_name = "../../../inputs/"+std::string(argv[1]);
+	in_f.open(in_f_name.c_str(), std::ifstream::in);
+	if(!in_f.is_open())
+	{
+		exit(2);
+	}
+	exit(0);
 }
