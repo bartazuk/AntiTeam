58a6e81620e10a1063c176a441ac362840840e7d
Alexandre Delove <alexandre.delove@gmail.com>
2017-02-23 20:16:30 +0100

    get line

--- finals/project_cpp/src/hashcode2017.cpp
+++ finals/project_cpp/src/hashcode2017.cpp
@@ -12,10 +12,10 @@ typedef struct latency_cache{
 	unsigned int latency_ms;
 }latency_cache;
 
-typedef struct request{
+typedef struct request_desc{
 	unsigned int id_video;
 	unsigned int nb_ask;
-}latency_cache;
+}request_desc;
 
 enum node_type {ENDPOINT, CACHE, DATA_CENTER};
 
@@ -28,13 +28,26 @@ typedef struct node {
 
 typedef struct endpoint{
 	unsigned int latency_datacenter;
-	std::vector<unsigned int, unsigned int> latency_cache;
-	std::vector<unsigned int, unsigned int> access;
+	std::vector<latency_cache> m_latency_cache;
+	std::vector<request_desc> access;
 }endpoint;
 
 typedef struct cache{
 	unsigned int cache_size;
 }cache;
+
+typedef struct layout{
+	unsigned int video_nb;
+	unsigned int end_nb;
+	unsigned int request_desc;
+	unsigned int cache_nb;
+	unsigned int cache_capacity;
+}layout;
+
+
+// ******************Variable static**********************
+
+static layout s_layout;
 
 
 
@@ -51,7 +64,24 @@ int main(int argc, char** argv)
 	in_f.open(in_f_name.c_str(), std::ifstream::in);
 	if(!in_f.is_open())
 	{
+		printf("ERROR\n");
 		exit(2);
 	}
+
+	std::string line;
+	std::getline(in_f, line);
+	std::stringstream m_ss(line);
+
+	//if whitespace between numbers
+	unsigned int *p_layout = reinterpret_cast<unsigned int*>(&s_layout);
+	while(m_ss >> *p_layout++)
+	{
+	}
+	m_ss << "";//erase stream
+	m_ss.clear();//clear eof flag
+	m_ss.seekg(0);//then set read ptr
+	printf("%u %u %u %u %u\n", s_layout.video_nb, s_layout.end_nb, s_layout.request_desc, s_layout.cache_nb, s_layout.cache_capacity);
+
+
 	exit(0);
 }
