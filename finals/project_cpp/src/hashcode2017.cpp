85bddb568de8435e0a582f19175ce903637043cf
Alexandre Delove <alexandre.delove@gmail.com>
2017-02-23 19:44:48 +0100

    class node

--- finals/project_cpp/src/hashcode2017.cpp
+++ finals/project_cpp/src/hashcode2017.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 #include <vector>
 
+enum node_type {ENDPOINT, CACHE, DATA_CENTER};
 
-//Struct
-
+typedef struct node {
+	enum node_type type;
+	unsigned int id;
+	std::vector<node *> link;
+	void * priv_data; // endpoint, cache etc...
+}node;
 
 typedef struct endpoint{
 	unsigned int latency_datacenter;
 	std::vector<unsigned int, unsigned int> latency_cache;
-	std::vector<unsigned int, unsigned int> ;
+	std::vector<unsigned int, unsigned int> access;
 }endpoint;
 
 typedef struct cache{
 	unsigned int cache_size;
 }cache;
-
 
 
 
